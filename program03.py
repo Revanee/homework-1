3eb1db96079f02cb8265199cb04fac4513ad2366
Hristo Kamenov <hkamenov123@gmail.com>
2018-10-23 20:15:34 +0200

    Stuff

--- program03.py
+++ program03.py
@@ -27,11 +27,77 @@ ATTENZIONE: quando caricate il file assicuratevi che sia nella codifica UTF8 (ad
 '''
 import time
 
-def es3(lista, testo):
-  resulting_words = []
-  most_common_word = ""
+seen_words = []
+times_seen = {}
+next_word_idx = 0
+most_common_word = ""
 
-  return (resulting_words, most_common_word)
+def seeWord(word):
+  if word in seen_words:
+    times_seen[word] += 1
+  else:
+    seen_words.append(word)
+    times_seen[word] = 1
+
+def getMostCommonWord():
+  max_occurances = 0
+  for word in seen_words:
+    if times_seen[word] > max_occurances:
+      max_occurances = times_seen[word]
+  most_common_words = []
+  for word in seen_words:
+    if times_seen[word] == max_occurances:
+      most_common_words.append(word)
+  return sorted(most_common_words, key=lambda x: x[0])[0]
+
+def checkWords(words, string):
+  global next_word_idx
+  for word in words:
+    matching = True
+    for i in range(len(word)):
+      print(i + next_word_idx)
+      print(word)
+      if word[i] != string[i + next_word_idx]:
+        matching = False
+        break
+
+    if matching:
+      seeWord(word)
+      next_word_idx += len(word)
+      break
+
+def es3(lista, testo):
+  global seen_words
+  seen_words = []
+  global times_seen
+  times_seen = {}
+  global next_word_idx
+  next_word_idx = 0
+
+  words_by_char = {}
+
+  for word in lista:
+    if len(words_by_char) < len(word):
+      words_by_char[len(word)] = [word]
+    else:
+      words_by_char[len(word)].append(word)
+
+  last_print = 0
+  while next_word_idx < len(testo):
+    remaining = len(testo) - next_word_idx
+    if next_word_idx - last_print > 100000:
+      print("remaining", remaining)
+      last_print = next_word_idx
+    
+    for words_key in words_by_char:
+      checkWords(words_by_char[words_key], testo)
+
+  # print("loop time", tot_time)
+
+  for word in seen_words:
+    lista.remove(word)
+
+  return (seen_words, getMostCommonWord())
 
 print(es3(["gatto", "cane", "topo"], "topogattotopotopogattogatto"))
 
